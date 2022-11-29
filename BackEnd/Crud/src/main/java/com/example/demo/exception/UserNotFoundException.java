5fbec9dd596a8fe5365e1dde6e0723385d99ab4d
Omkar Utake <108263863+utake54@users.noreply.github.com>
2022-11-29 23:30:23 +0530

    Update UserNotFoundException.java

--- BackEnd/Crud/src/main/java/com/example/demo/exception/UserNotFoundException.java
+++ BackEnd/Crud/src/main/java/com/example/demo/exception/UserNotFoundException.java
@@ -1,10 +1,7 @@
 package com.example.demo.exception;
 
 public class UserNotFoundException extends RuntimeException {
-/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+
 
 public UserNotFoundException(Long id) {
 	super("this id is not present" +id);
